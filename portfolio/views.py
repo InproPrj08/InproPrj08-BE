a30fcd7a800596f0349e6f6cad580ec61664e67c
codmsdlrltgjqm <chaeni1218@naver.com>
2023-11-15 20:26:35 +0900

    Feat:portfolio 게시글 작성

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Portfolio
+from django.views.generic import ListView
 # Create your views here.
+
+class PortfolioList(ListView):
+    model = Portfolio
+    ordering = '-pk'
+
